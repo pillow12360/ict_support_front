698f0e3fef4f776e4a87042acd30a777b1338dfa
pillow12360 <ehdcks1256@naver.com>
2024-01-18 18:59:27 +0900

    Home 컴포넌트 UI 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,33 +31,40 @@ const Home = () => {
 
   return (
     <div className="container mt-5">
-      <h1 className="mb-3">ICT 지원실 민원 처리</h1>
-      <p>이곳에서는 다양한 ICT 관련 민원을 접수하고 처리합니다.</p>
-      <p>
-        문제 해결을 위한 자주 묻는 질문, 사용자 가이드 등을 찾아볼 수 있습니다.
-      </p>
+      <div className="row">
+        <div className="col">
+          <h1 className="mb-3">ICT 지원실 민원 처리</h1>
+          <p className="lead">
+            이곳에서는 다양한 ICT 관련 민원을 접수하고 처리합니다.
+          </p>
+          <p>
+            문제 해결을 위한 자주 묻는 질문, 사용자 가이드 등을 찾아볼 수
+            있습니다.
+          </p>
 
-      <Link to="/authscreen" className="btn btn-secondary mr-2">
-        로그인/회원가입
-      </Link>
-      <div>
-        <Link to="/complaints" className="btn btn-light">
-          민원 접수
-        </Link>
+          <div className="mb-3">
+            <Link to="/authscreen" className="btn btn-secondary mr-2">
+              로그인/회원가입
+            </Link>
+            <Link to="/complaints" className="btn btn-light mr-2">
+              민원 접수
+            </Link>
+            <button className="btn btn-info" onClick={() => setShowModal(true)}>
+              민원 조회
+            </button>
+          </div>
+        </div>
+      </div>
 
-        <button
-          className="btn btn-info mr-2"
-          onClick={() => setShowModal(true)}
-        >
-          민원 조회
-        </button>
+      {showModal && (
         <Modal show={showModal} onClose={() => setShowModal(false)}>
           <ComplaintTemplate>
             <ComplaintList complaints={complaints} />
           </ComplaintTemplate>
         </Modal>
-      </div>
+      )}
     </div>
   );
 };
+
 export default Home;
