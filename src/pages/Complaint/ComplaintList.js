f957e0729abba813dfa56fd733d7cd45e0bd2b08
pillow12360 <ehdcks1256@naver.com>
2024-01-26 13:13:59 +0900

    프록시 서버 해제, ComplaintList 스타일 지정

--- src/pages/Complaint/ComplaintList.js
+++ src/pages/Complaint/ComplaintList.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import db from '../../firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { AuthContext } from '../../AuthContext';
+import '../../style/ComplaintList.scss';
 
 // 유저 id 값으로 접수한 민원 조회 컴포넌트
 
@@ -33,12 +34,13 @@ const ComplaintList = () => {
   }, [currentUserId]);
 
   return (
-    <div>
+    <div className="complaint-list-container">
       <h1>접수한 민원 조회</h1>
       {complaints.map((complaint) => (
-        <div key={complaint.id}>
+        <div key={complaint.id} className="complaint">
           <p>민원 제목: {complaint.title}</p>
-          {/* 여기에 더 많은 민원 정보를 렌더링할 수 있습니다 */}
+          <p>건물: {complaint.building}</p>
+          {/* 추가 민원 정보 */}
         </div>
       ))}
     </div>
