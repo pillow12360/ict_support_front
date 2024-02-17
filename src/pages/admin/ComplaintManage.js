169520f5afafab830ae06a84203fd16d366d48ab
pillow12360 <ehdcks1256@naver.com>
2024-02-17 23:32:14 +0900

    MUI, recharts 라이브러리 설치, 관리자 컴포넌트 구현 중, 에러 조치 필요

--- src/pages/admin/ComplaintManage.js
+++ src/pages/admin/ComplaintManage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Dashboard from './Dashboard';
+import ComplaintsListManage from './ComplaintsListManage';
 
 const ComplaintManage = () => {
   return (
     <div>
       <h1>관리자 페이지 입니다.</h1>
+      <Dashboard />
+      <ComplaintsListManage />
     </div>
   );
 };
-
 export default ComplaintManage;
---
+++ src/pages/admin/ComplaintsListManage.js
@@ -0,0 +1,49 @@
+// src/components/ComplaintsList.js
+import React, { useState, useEffect } from 'react';
+import { db } from '../../firebase';
+import { collection, query, getDocs } from 'firebase/firestore';
+
+const ComplaintsListManage = () => {
+  const [complaints, setComplaints] = useState([]);
+
+  useEffect(() => {
+    const fetchComplaints = async () => {
+      try {
+        const q = query(collection(db, 'complaints'));
+        const querySnapshot = await getDocs(q);
+        const complaintsData = querySnapshot.docs.map((doc) => {
+          // Timestamp를 Date 객체로 변환
+          const timestamp = doc.data().timestamp;
+          const date = timestamp ? timestamp.toDate() : new Date();
+          // 원하는 날짜 형식으로 변환 (예: '2024-01-30')
+          const formattedDate = date.toLocaleDateString('ko-KR');
+          return {
+            id: doc.id,
+            ...doc.data(),
+            // 변환된 날짜를 사용
+            timestamp: formattedDate,
+          };
+        });
+        setComplaints(complaintsData);
+      } catch (error) {
+        console.error('Error fetching complaints: ', error);
+      }
+    };
+    fetchComplaints();
+  }, []);
+
+  return (
+    <div>
+      <h2>모든 민원</h2>
+      <ul>
+        {complaints.map((complaint) => (
+          <li key={complaint.id}>
+            {complaint.title} - {complaint.status}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ComplaintsListManage;
